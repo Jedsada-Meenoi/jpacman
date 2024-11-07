46968deb6303c6f821c828d8513dffc12985c604
Jedsada Meenoi <142957083+Jedsada-Meenoi@users.noreply.github.com>
2024-11-07 13:54:48 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,8 +1,5 @@
 package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
